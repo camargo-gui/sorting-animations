854068c1ddd5f1f0ce8c08e5e0067b6aa736c4cf
camargo-rubcube <guilherme.camargo@rubcube.com>
2024-03-22 01:42:44 -0300

    feat: variable indicator

--- src/main/java/org/example/sortinganimations/Principal.java
+++ src/main/java/org/example/sortinganimations/Principal.java
@@ -2,11 +2,14 @@ package org.example.sortinganimations;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
 
@@ -19,14 +22,14 @@ public class Principal extends Application {
     Button botao_inicio;
     private Button vet[];
     private int TL = 10;
-
     private VBox codeContainer = new VBox(5);
-    private int highlightedLine = 0;
-
     private int [] inMemory = new int[1000];
-
     private Task tasks[] = new Task[1000];
     private int TaskTL = 0;
+    private Label LabelI = new Label();
+    private Label LabelJ = new Label();
+    private Label LabelAux = new Label();
+    private Label LabelDist = new Label();
 
     public static void main(String[] args) {
         launch(args);
@@ -64,6 +67,7 @@ public class Principal extends Application {
         for(String line : codeLines){
             Label label = new Label(line);
             label.setFont(new Font(20));
+            label.setStyle("-fx-text-fill: #FFFFFF");
             codeContainer.getChildren().add(label);
         }
     }
@@ -71,7 +75,7 @@ public class Principal extends Application {
     private void clearHighlight(){
         for(int i = 0; i < codeContainer.getChildren().size(); i++){
             Label label = (Label) codeContainer.getChildren().get(i);
-            label.setStyle("-fx-background-color: #FFFFFF");
+            label.setStyle("-fx-text-fill: #FFFFFF");
         }
     }
 
@@ -81,7 +85,7 @@ public class Principal extends Application {
             protected Void call() {
                 clearHighlight();
                 Label label = (Label) codeContainer.getChildren().get(line);
-                label.setStyle("-fx-background-color: yellow");
+                label.setStyle("-fx-text-fill: #FFFF00");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -100,7 +104,7 @@ public class Principal extends Application {
         stage.setTitle("Pesquisa e Ordenacao");
         pane = new AnchorPane();
         botao_inicio = new Button();
-        botao_inicio.setLayoutX(10);
+        botao_inicio.setLayoutX(100);
         botao_inicio.setLayoutY(100);
         botao_inicio.setText("Shell sort");
         botao_inicio.setOnAction(e -> {
@@ -123,9 +127,22 @@ public class Principal extends Application {
             pane.getChildren().add(vet[i]);
         }
         renderCodeBox();
+        Rectangle frame = new Rectangle();
+        frame.setWidth(500);
+        frame.setHeight(700);
+        frame.setFill(Color.BLACK);
+        frame.setLayoutX(950);
+        frame.setLayoutY(80);
+        frame.setArcWidth(20);
+        frame.setArcHeight(20);
+        codeContainer.setLayoutX(0);
+        codeContainer.setLayoutY(0);
+        Group group = new Group();
+        group.getChildren().addAll(frame, codeContainer);
         codeContainer.setLayoutX(1000);
         codeContainer.setLayoutY(100);
-        pane.getChildren().add(codeContainer);
+        pane.getChildren().add(group);
+        inicializarLabels();
         Scene scene = new Scene(pane, 1280, 1080);
         stage.setScene(scene);
         stage.show();
@@ -159,28 +176,33 @@ public class Principal extends Application {
     public void shell_sort() throws InterruptedException {
         highlightLine(1);
         int dist = 1, j, aux, aux2, acumulatedDist;
-        boolean trocou = false;
         while (dist < TL) {
             highlightLine(3);
             dist = dist * 3 + 1;
             highlightLine(4);
+            atribuiValorAoLabel(LabelDist, dist * 3 + 1);
         }
         highlightLine(5);
         dist = dist / 3;
         highlightLine(6);
+        atribuiValorAoLabel(LabelDist, dist);
+
 
         while (dist > 0) {
             highlightLine(8);
             highlightLine(9);
+            atribuiValorAoLabel(LabelI, dist);
             for (int i = dist; i < TL; i++) {
                 highlightLine(9);
                 vet = reorganizeVet();
                 j = i;
+                atribuiValorAoLabel(LabelAux, inMemory[i]);
                 highlightLine(10);
                 aux2 = inMemory[i];
                 separaAux(j);
                 acumulatedDist = 0;
                 highlightLine(11);
+                atribuiValorAoLabel(LabelJ, i);
                 aux = j;
                 highlightLine(12);
                 colorir(j, j - dist, "yellow");
@@ -192,6 +214,7 @@ public class Principal extends Application {
                     inMemory[j] = inMemory[j - dist];
                     acumulatedDist += dist;
                     highlightLine(14);
+                    atribuiValorAoLabel(LabelJ, j - dist);
                     j -= dist;
                 }
                 highlightLine(12);
@@ -201,8 +224,10 @@ public class Principal extends Application {
                 highlightLine(16);
                 inMemory[j] = aux2;
                 retornaOAux(aux, acumulatedDist);
+                atribuiValorAoLabel(LabelI, i+1);
             }
             highlightLine(18);
+            atribuiValorAoLabel(LabelDist, dist/3);
             dist = dist / 3;
         }
         colorSuccess();
@@ -314,6 +339,42 @@ public class Principal extends Application {
         TaskTL++;
     }
 
+    private void inicializarLabels(){
+        LabelI.setFont(new Font(20));
+        LabelI.setLayoutY(350);
+        LabelI.setLayoutX(850);
+        LabelI.setText("i = 0");
+        LabelJ.setFont(new Font(20));
+        LabelJ.setLayoutY(400);
+        LabelJ.setLayoutX(850);
+        LabelJ.setText("j = 0");
+        LabelAux.setFont(new Font(20));
+        LabelAux.setLayoutY(450);
+        LabelAux.setLayoutX(850);
+        LabelAux.setText("aux = 0");
+        LabelDist.setFont(new Font(20));
+        LabelDist.setLayoutY(500);
+        LabelDist.setLayoutX(850);
+        LabelDist.setText("dist = 1");
+        pane.getChildren().add(LabelI);
+        pane.getChildren().add(LabelJ);
+        pane.getChildren().add(LabelAux);
+        pane.getChildren().add(LabelDist);
+    }
+
+    private void atribuiValorAoLabel(Label label, int valor){
+        Task <Void> task = new Task<Void>() {
+            @Override
+            protected Void call() {
+                String textoAtual = label.getText();
+                String[] partes = textoAtual.split("=");
+                Platform.runLater(() -> label.setText(partes[0].trim() + " = " + valor));
+                return null;
+            }
+        };
+        tasks[TaskTL] = task;
+        TaskTL++;
+    }
     private void runTasks(){
         Task <Void> task = new Task<Void>() {
             @Override
@@ -326,47 +387,5 @@ public class Principal extends Application {
         };
         Thread thread = new Thread(task);
         thread.start();
-    }
-    public void move_botoes(int j, int k)
-    {
-        Task<Void> task = new Task<Void>(){
-            @Override
-            protected Void call() {
-                for (int i = 0; i < 10; i++) {
-                    Platform.runLater(() -> vet[j].setLayoutY(vet[j].getLayoutY() + 5));
-                    Platform.runLater(() -> vet[k].setLayoutY(vet[k].getLayoutY() - 5));
-                    try {
-                        Thread.sleep(25);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                int dist = k - j;
-                for (int i = 0; i < 16 * dist; i++) {
-                    Platform.runLater(() -> vet[j].setLayoutX(vet[j].getLayoutX() + 5));
-                    Platform.runLater(() -> vet[k].setLayoutX(vet[k].getLayoutX() - 5));
-                    try {
-                        Thread.sleep(25);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                for (int i = 0; i < 10; i++) {
-                    Platform.runLater(() -> vet[j].setLayoutY(vet[j].getLayoutY() - 5));
-                    Platform.runLater(() -> vet[k].setLayoutY(vet[k].getLayoutY() + 5));
-                    try {
-                        Thread.sleep(25);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                Button aux = vet[j];
-                vet[j] = vet[k];
-                vet[k] = aux;
-                return null;
-            }
-        };
-        tasks[TaskTL] = task;
-        TaskTL++;
     }
 }
